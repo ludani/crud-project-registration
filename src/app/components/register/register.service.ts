6e47b370e1bf0d6ddef65b168457fd354709ac2e
Lucas Daniel Alves Vieira <ldanivieira@gmail.com>
2021-01-09 22:30:53 -0400

    input complet

--- src/app/components/register/register.service.ts
+++ src/app/components/register/register.service.ts
@@ -10,8 +10,8 @@ import { Observable } from 'rxjs';
 })
 export class RegisterService {
   //PASSANDO O PARAMETRO POST AQUI
-  baseUrl = 'https://5ff8b52a17386d0017b51780.mockapi.io/api/project';
 
+  baseUrl = 'https://5ff8b52a17386d0017b51780.mockapi.io/api/project';
   constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
  //EXEMPLO
   showOnConsole(msg: string): void {
